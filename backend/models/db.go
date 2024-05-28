af7531166ca2047f14a365ef80e2c486059423f4
Safalgautam3636 <seeram3636@gmail.com>
2024-05-27 20:22:00 -0400

    Format code

--- backend/models/db.go
+++ backend/models/db.go
@@ -5,20 +5,20 @@ import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
+
 var DB *gorm.DB
 
+func ConnectDatabase(host string, username string, password string, name string, port string) {
 
-func ConnectDatabase(host string,username string,password string,name string,port string){
-  
-  dsn:=fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, username, password, name, port)
-  database,err:=gorm.Open(postgres.Open(dsn), &gorm.Config{})
-  if err!=nil{
-    panic("Failed to connect to database!")
-  }
-  err=database.AutoMigrate(&Job{})
-  if err!=nil{
-    return
-  }
-  DB=database
-  fmt.Println(DB)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, username, password, name, port)
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database!")
+	}
+	err = database.AutoMigrate(&Job{})
+	if err != nil {
+		return
+	}
+	DB = database
+	fmt.Println(DB)
 }
