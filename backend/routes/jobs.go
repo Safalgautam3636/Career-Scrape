3ee2920ccbeee940c11454cf3950d03c2f5a4dd7
Safalgautam3636 <seeram3636@gmail.com>
2024-06-13 17:39:44 -0400

    Update job routes

--- backend/routes/jobs.go
+++ backend/routes/jobs.go
@@ -1,8 +1,6 @@
 package routes
 
 import (
-	"fmt"
-	"net/http"
 	"backend/controllers"
 	"github.com/gin-gonic/gin"
 )
@@ -10,34 +8,9 @@ import (
 func RegisterJobRoutes(route *gin.Engine) {
 	jobs := route.Group("/jobs")
 	jobs.GET("/", controllers.GetAllJobs)
-	jobs.GET("/:id", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "this is a single job",
-		})
-	})
-	jobs.POST("/create", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "New job created",
-		})
-	})
-	jobs.DELETE("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "All jobs deleted",
-		})
-	})
-	jobs.DELETE("/:id", func(c *gin.Context) {
-		id := c.Param("id")
-		message := fmt.Sprintf("job with id %s deleted", id)
-		c.JSON(http.StatusOK, gin.H{
-
-			"message": message,
-		})
-	})
-	jobs.PUT("/:id", func(c *gin.Context) {
-		id := c.Param("id")
-		message := fmt.Sprintf("Job with id %s is updated", id)
-		c.JSON(http.StatusOK, gin.H{
-			"message": message,
-		})
-	})
+	jobs.GET("/:id", controllers.GetJobById)
+	jobs.POST("/create",controllers.CreateJob)
+	jobs.DELETE("/", controllers.DeleteJobs)
+	jobs.DELETE("/:id", controllers.DeleteJobById)
+	jobs.PUT("/:id", controllers.UpdateJobById)
 }
