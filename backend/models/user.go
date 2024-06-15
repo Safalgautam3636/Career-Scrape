5ae9a19f2b8098e4384e14a0e49db4d28cd204a5
Safalgautam3636 <seeram3636@gmail.com>
2024-06-15 13:36:16 -0400

    Fix user schema

--- backend/models/user.go
+++ backend/models/user.go
@@ -1,14 +1,16 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
-
-type User struct{
+type User struct {
 	gorm.Model
-	ID              uuid.UUID `json:"id" gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
+	ID uuid.UUID `json:"id" gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
 	// ID string `json:"id" gorm:"primary_key"`
 	Username string `json:"username" gorm:"size:255;not null;unique"`
-	Email string `json:"email" gorm:"unique;not null"`
+	Email    string `json:"email" gorm:"unique;not null"`
 	Password string `json:"password" gorm:"unique;not null"`
 	IsAdmin  bool   `json:"isAdmin" gorm:"default:false"`
 }
