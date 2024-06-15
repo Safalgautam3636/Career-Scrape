bc9c43461a3d7eba0f69f0eae3060b5f4ead3d4c
Safalgautam3636 <seeram3636@gmail.com>
2024-06-15 13:35:50 -0400

    Update user schema

--- backend/models/user.go
+++ backend/models/user.go
@@ -5,7 +5,8 @@ import "gorm.io/gorm"
 
 type User struct{
 	gorm.Model
-	ID string `json:"id" gorm:"primary_key"`
+	ID              uuid.UUID `json:"id" gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
+	// ID string `json:"id" gorm:"primary_key"`
 	Username string `json:"username" gorm:"size:255;not null;unique"`
 	Email string `json:"email" gorm:"unique;not null"`
 	Password string `json:"password" gorm:"unique;not null"`
