777ac9e31cc6ffa3486775178a2a3f29d5f3c7ae
Safalgautam3636 <seeram3636@gmail.com>
2024-06-16 01:52:50 -0400

    Add boilerplate for scraping route

--- backend/controllers/scraping.go
+++ backend/controllers/scraping.go
@@ -1,1 +1,30 @@
 package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func StartAllSpiders(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Started all scrappers",
+	})
+}
+func StartSpiderWithId(c *gin.Context) {
+	spider := c.Param("spiderId")
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Started spider:" + " " + spider,
+	})
+}
+func StopAllSpiders(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Stopped all Scraping",
+	})
+}
+func StopSpiderWithId(c *gin.Context) {
+	spider := c.Param("spiderId")
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Stopped spider:" + " " + spider,
+	})
+}
--- backend/routes/scraping.go
+++ backend/routes/scraping.go
@@ -1,32 +1,21 @@
 package routes
 
 import (
+	"backend/controllers"
 	"fmt"
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func RegisterScrapingRoutes(route *gin.Engine) {
 	scrape := route.Group("/scrape")
-	scrape.POST("/start", func(c *gin.Context) {
-		go startScraping()
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Started all scrappers",
-		})
-
-	})
-	scrape.POST("/:spider", func(c *gin.Context) {
-		spider := c.Param("spider")
-		go startSingleSpider(spider)
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Started spider:" + " " + spider,
-		})
-	})
-}
-func startScraping() {
-	fmt.Println("Started all scraping...")
-}
-func startSingleSpider(spider string) {
-	message := fmt.Sprintf("Spider with name: %s started..", spider)
-	fmt.Println(message)
+	//start all spiders
+	scrape.POST("/start", controllers.StartAllSpiders)
+	//start one spider
+	scrape.POST("/start/:spiderId", controllers.StartSpiderWithId)
+	//stop all spiders
+	scrape.POST("/stop", controllers.StopAllSpiders)
+	//stop one spider
+	scrape.POST("/stop/:spiderId", controllers.StopSpiderWithId)
 }
