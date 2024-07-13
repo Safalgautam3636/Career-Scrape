314f44c5dd64a6ca9878978f46c8dd688b9fb3f8
Safalgautam3636 <seeram3636@gmail.com>
2024-07-13 14:16:37 -0400

    Update Job Schema

--- backend/models/job.go
+++ backend/models/job.go
@@ -12,16 +12,18 @@ import (
 type Job struct {
 	gorm.Model
 	ID              uuid.UUID `json:"id" gorm:"type:uuid;primary_key;default:uuid_generate_v4()"`
-	Role            string    `json:"role"`
-	Link            string    `json:"link"`
-	Address         string    `json:"address"`
+	Title           string    `json:"job_title"`
+	Link            string    `json:"job_link"`
+	Location        string    `json:"job_location"`
+	DatePosted      string 	  `json:"job_posted"` // 4 hrs ago...
 	CompanyName     string    `json:"company_name"`
-	DatePosted      time.Time `json:"posted_date"`
-	JobType         string    `json:"job_type"` //remote/onsite/hybrid
+	ExactDate       time.Time `json:"exact_date"` // actual date
+	JobType         string    `json:"job_type"` //fulltime/contract
+	CompanyDomain   string    `json:"company_domain"`     
+	JobLevel        string    `json:"job_level"`
+	CompanyLink     string     `json:"company_link"`   
 	Description     string    `json:"description"`
-	Salary          string    `json:"salary"`
 	PulledTimeStamp time.Time `json:"pulled_date" gorm:"default:CURRENT_TIMESTAMP"`
-	ExperienceLevel string    `json:"experience_level"`
 	// CreatedAt       time.Time `json:"created_at,omitempty"`
 	// UpdatedAt       time.Time `json:"updated_at,omitempty"`
 	// DeletedAt       time.Time `json:"deleted_at,omitempty"`
